d6bd4e3e8ebdf6788626e2de1b615413bcafebe9
William Brown <brownwc86@gmail.com>
2018-09-17 16:00:41 -0400

    Changed webCrawler.py/ still learning how to pull pages.

--- PythonFiles/webCrawler.py
+++ PythonFiles/webCrawler.py
@@ -8,37 +8,31 @@
                 dates and times. This information will be stored with the information containing 
                 the bands/ ticket price/ date/ time/ and links to the bands webpages/music. 
 
-    TODO:       1. Set up and test the program searching one venues page. 
+    TODO:       1. Set up and test the program searching one venues page.
+                    Venues to test with, 
+                        The Fillmore - uses LiveNation for calender information
+                        The Evening Muse - 
+                        Visulite Theatre - 
+                        The Rabbit Hole - 
+                        AvidXchange Music Factory - 
+                        The Milestone Club - 
+                        Neighborhood Theatre - 
+                        Petra's Bar - 
+                        PNC Music Pavilion - 
+                        The Underground - 
+                        Charlotte Metro Credit Union Amphitheatre - 
+                        Tin Roof - 
                 2. Set up and access a database for storage of the information gathered. 
                 
 
 """
-from requests import get
+import requests
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 
-def get_site(url):
+r = requests.get("http://www.fillmorenc.com")
+print("Status", r.status_code)
 
-    try:
-        with closing(get(url, stream=True)) as resp:
-            if is_good_response(resp):
-                return resp.content
-            else:
-                return None
-
-    except RequestException as e:
-        log_error('Error during requests to {0}: {1}'.format(url,str(e)))
-        return None
-
-def is_good_response(resp):
-
-    content_type = resp.headers['Content-Type'].lower()
-    return (resp.status_code == 200 
-            and content_type is not None 
-            and content_type.find('html') > -1)
-
-
-def log_error(e):
-    
-    print(e)
+f = open("./page.html", "w+")
+f.write(r.text)
