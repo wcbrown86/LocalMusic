e0104b190930ffecebb9b3c5259354b463513628
William Brown <brownwc86@gmail.com>
2019-04-20 00:42:43 -0400

    Added notes to LocalVenue, and GenerateVenues.

--- Backend/JavaFiles/LocalMusicServerApplet/src/main/java/LocalMusicServerApplet/GenerateVenues.java
+++ Backend/JavaFiles/LocalMusicServerApplet/src/main/java/LocalMusicServerApplet/GenerateVenues.java
@@ -3,6 +3,7 @@ package LocalMusicServerApplet;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.ticketmaster.api.discovery.DiscoveryApi;
@@ -16,13 +17,22 @@ import com.ticketmaster.discovery.model.Venues;
  * <h3>
  *		About:
  * </h3>
+ * 
+ * 			This Class is used to gather updated, or create new information from venues. The information is pulled
+ * 			from the used APIs that are provided by the Event companies like LiveNation. 
  *
  *
  * @author 		William Brown
  * @copyright 	Apr 19, 2019
  * @version		0.0.1
  * 
- * TODO
+ * TODO : 
+ * 			1. Currently working on using the Live Nation API to gather all the venue information that 
+ * 			   is stored in there API. 
+ * 			2. Create a function that will pulled saved venue information from the database and then check
+ * 			   for updates.
+ * 			3. Add search function for a different Event API. More research is needed for the next API system
+ * 			   to use. 
  *
  */
 
@@ -31,9 +41,20 @@ public class GenerateVenues {
 	
 	static final String LIVENATIONAPIKEY = "huk5WDVW6kgR9bt0WVXGGHExyyAF5UlX";
 	
-	public ArrayList<LocalVenue> gatherLiveNationVenues(String countryCode){
+	
+	/**
+	 * In this function, the application will take a given Postal Code and will gather all the 
+	 * venues in this postal code. Then created a LocalVenue object and add this object to a LinkedList.
+	 * This linkedList is returned to the calling method to be stored for later use. This function is meant
+	 * to be used to update the list with changes made by the venues. Will be ran monthly as a part of the 
+	 * application upkeep.
+	 * 
+	 * @param countryCode : A String that represents the Postal Code or Zip code that needs to be checked for venues.
+	 * @return : A linked list that can be converted to a JSON file for later use, or used to update the SQL server. 
+	 */
+	public LinkedList<LocalVenue> gatherLiveNationVenues(String countryCode){
 		
-		ArrayList<LocalVenue> venueArrayList = new ArrayList<LocalVenue>();
+		LinkedList<LocalVenue> fullVenueList = new LinkedList<LocalVenue>();
 		
 		DiscoveryApi apiConnection = new DiscoveryApi(LIVENATIONAPIKEY);
 		
@@ -64,7 +85,7 @@ public class GenerateVenues {
 												 venue.getId(),
 												 "LiveNation");
 				
-				venueArrayList.add(temp);
+				fullVenueList.add(temp);
 				
 			}
 			
@@ -75,10 +96,10 @@ public class GenerateVenues {
 		
 		
 		
-		if(venueArrayList.isEmpty())
+		if(fullVenueList.isEmpty())
 			return null;
 		else
-			return venueArrayList;
+			return fullVenueList;
 		
 		
 	}
