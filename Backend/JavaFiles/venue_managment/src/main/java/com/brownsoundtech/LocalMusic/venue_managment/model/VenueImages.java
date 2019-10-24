2d7187f3999127793db419b4d5094283cf5305ab
William Brown <brownwc86@gmail.com>
2019-10-24 18:20:36 -0400

    Added ImageService and started testing this service.

--- Backend/JavaFiles/venue_managment/src/main/java/com/brownsoundtech/LocalMusic/venue_managment/model/VenueImages.java
+++ Backend/JavaFiles/venue_managment/src/main/java/com/brownsoundtech/LocalMusic/venue_managment/model/VenueImages.java
@@ -1,12 +1,62 @@
 package com.brownsoundtech.LocalMusic.venue_managment.model;
 
+import java.net.URL;
+
 public class VenueImages {
 
-	//TODO: Implement toString override. 
-	@Override
-	public String toString() {
-		
-		return "";
+	private String ratio;
+	private URL url;
+	private int width, height;
+	
+	/**
+	 * @return the ratio
+	 */
+	public String getRatio() {
+		return ratio;
 	}
+	/**
+	 * @param ratio the ratio to set
+	 */
+	public void setRatio(String ratio) {
+		this.ratio = ratio;
+	}
+	/**
+	 * @return the url
+	 */
+	public URL getUrl() {
+		return url;
+	}
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+	/**
+	 * @return the width
+	 */
+	public int getWidth() {
+		return width;
+	}
+	/**
+	 * @param width the width to set
+	 */
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	/**
+	 * @return the height
+	 */
+	public int getHeight() {
+		return height;
+	}
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+
 	
 }
---
+++ Backend/JavaFiles/venue_managment/src/main/java/com/brownsoundtech/LocalMusic/venue_managment/service/VenueImageService.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.brownsoundtech.LocalMusic.venue_managment.service;
+
+import java.net.URL;
+
+import org.apache.commons.validator.routines.UrlValidator;
+
+import com.ticketmaster.discovery.model.Image;
+
+/**
+ * @author brown
+ *
+ */
+public class VenueImageService {
+	
+	public VenueImageService(Image image) {
+		
+	}
+	
+	public boolean isNull(Object object) {
+		
+		if(object == null)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isURL(String url) {
+		
+		UrlValidator urlValid = new UrlValidator();
+		
+		return urlValid.isValid(url);
+	}
+
+}
