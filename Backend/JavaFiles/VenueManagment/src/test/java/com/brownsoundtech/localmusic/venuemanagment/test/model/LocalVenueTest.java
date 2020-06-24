f9f9bba4c86f022e3e8bd1aa7c20d93307f7a678
William Brown <brownwc86@gmail.com>
2020-06-24 11:29:55 -0400

    Added more test to LocalVenue Model.

--- Backend/JavaFiles/VenueManagment/src/test/java/com/brownsoundtech/localmusic/venuemanagment/test/model/LocalVenueTest.java
+++ Backend/JavaFiles/VenueManagment/src/test/java/com/brownsoundtech/localmusic/venuemanagment/test/model/LocalVenueTest.java
@@ -13,8 +13,11 @@ import com.brownsoundtech.localmusic.venuemanagment.model.LocalVenue;
  *
  */
 class LocalVenueTest {
+	
 	LocalVenue validVenue, emptyVenue, nullVenue;
+	
 	String notValidURL = "No Valid URL Was Provided.";
+	
 	String notValidString = "No Imformation Provided";
 	
 	
@@ -24,7 +27,7 @@ class LocalVenueTest {
 		validVenue = new LocalVenue();
 		
 		validVenue.setAddress1("2505 Washington Rd.");
-		validVenue.setAddress2(" ");
+		validVenue.setAddress2("Test Address 2 Imformation");
 		validVenue.setCity("Augusta");
 		validVenue.setState("Georgia");
 		validVenue.setZip("30904");
@@ -119,6 +122,112 @@ class LocalVenueTest {
 		assertEquals(notValidString, emptyVenue.validatedString(" "));
 		
 	}
+	
+	@Test 
+	void testGetNameValidInformation() {
+		
+		assertEquals("Augusta National Golf Course", validVenue.getName());
+		
+	}
+	
+	@Test
+	void testGetNameNullInformation() {
+		
+		assertEquals(notValidString, nullVenue.getName());
+		
+	}
+	
+	@Test
+	void testGetNameEmptyInformation() {
+		
+		assertEquals(notValidString, emptyVenue.getName());
+		
+	}
+	
+	@Test 
+	void testGetAddress1ValidInformation() {
+		
+		assertEquals("2505 Washington Rd.", validVenue.getAddress1());
+		
+	}
+	
+	@Test
+	void testGetAddress1NullInformation() {
+		
+		assertEquals(notValidString, nullVenue.getAddress1());
+		
+	}
+	
+	@Test
+	void testGetAddress1EmptyInformation() {
+		
+		assertEquals(notValidString, emptyVenue.getAddress1());
+		
+	}
+	
+	@Test 
+	void testGetAddress2ValidInformation() {
+		
+		assertEquals("Test Address 2 Imformation", validVenue.getAddress2());
+		
+	}
+	
+	@Test
+	void testGetAddress2NullInformation() {
+		
+		assertEquals(notValidString, nullVenue.getAddress2());
+		
+	}
+	
+	@Test
+	void testGetAddress2EmptyInformation() {
+		
+		assertEquals(notValidString, emptyVenue.getAddress2());
+		
+	}
+	
+	@Test 
+	void testGetCityValidInformation() {
+		
+		assertEquals("Augusta", validVenue.getCity());
+		
+	}
+	
+	@Test
+	void testGetCityNullInformation() {
+		
+		assertEquals(notValidString, nullVenue.getCity());
+		
+	}
+	
+	@Test
+	void testGetCityEmptyInformation() {
+		
+		assertEquals(notValidString, emptyVenue.getCity());
+		
+	}
+	
+	@Test 
+	void testGetStateValidInformation() {
+		
+		assertEquals("Georgia", validVenue.getState());
+		
+	}
+	
+	@Test
+	void testGetStateNullInformation() {
+		
+		assertEquals(notValidString, nullVenue.getState());
+		
+	}
+	
+	@Test
+	void testGetStateEmptyInformation() {
+		
+		assertEquals(notValidString, emptyVenue.getState());
+		
+	}
+	
 	
 	@AfterEach
 	void clearMemory() {
