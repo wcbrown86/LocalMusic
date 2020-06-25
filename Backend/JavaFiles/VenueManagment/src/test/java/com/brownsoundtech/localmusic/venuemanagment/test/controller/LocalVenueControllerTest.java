80a0bbb68de996c86296acdcdc237538e678efc3
William Brown <brownwc86@gmail.com>
2020-06-25 16:32:40 -0400

    Setup test classed for controller and view.

---
+++ Backend/JavaFiles/VenueManagment/src/test/java/com/brownsoundtech/localmusic/venuemanagment/test/controller/LocalVenueControllerTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.brownsoundtech.localmusic.venuemanagment.test.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author brown
+ *
+ */
+class LocalVenueControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Backend/JavaFiles/VenueManagment/src/test/java/com/brownsoundtech/localmusic/venuemanagment/test/view/LocalVenueViewTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.brownsoundtech.localmusic.venuemanagment.test.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author brown
+ *
+ */
+class LocalVenueViewTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
