77a0bce5971d8848f99c968d0466b38488e9322c
William Brown <brownwc86@gmail.com>
2020-06-25 15:51:00 -0400

    Completed Controller Class for LocalVenue, Started View Class.

---
+++ Backend/JavaFiles/VenueManagment/src/main/java/com/brownsoundtech/localmusic/venuemanagment/controller/LocalVenueController.java
@@ -0,0 +1,139 @@
+/**
+ * 
+ */
+package com.brownsoundtech.localmusic.venuemanagment.controller;
+
+import com.brownsoundtech.localmusic.venuemanagment.model.LocalVenue;
+import com.brownsoundtech.localmusic.venuemanagment.view.LocalVenueView;
+
+/**
+ * @author brown
+ *
+ */
+public class LocalVenueController {
+	
+	LocalVenue model;
+	LocalVenueView view;
+	
+	 public LocalVenueController(LocalVenue model, LocalVenueView view) {
+		 
+		 this.model = model;
+		 this.view = view;
+		 
+	 }
+	 
+	 public void updateLocalVenueView() {
+		 
+		 view.printVenueInformation(this);
+		 
+	 }
+	 
+	 public void setLocalVenueName(String name) {
+		 
+		 model.setName(name);
+		 
+	 }
+	 
+	 public String getLocalVenueName() {
+		 
+		 return model.getName();
+		 
+	 }
+	 
+	 public void setLocalVenueAddress1(String address) {
+		 
+		 model.setAddress1(address);
+		 
+	 }
+	 
+	 public String getLocalVenueAddress1() {
+		 
+		 return model.getAddress1();
+		 
+	 }
+	 
+	 public void setLocalVenueAddress2(String Address) {
+		 
+		 model.setAddress2(Address);
+		 
+	 }
+	 
+	 public String getLocalVenueAddress2() {
+		 
+		 return model.getAddress2();
+		 
+	 }
+	 
+	 public void setLocalVenueCity(String city) {
+		 
+		 model.setCity(city);
+		 
+	 }
+	 
+	 public String getLocalVenueCity() {
+		 
+		 return model.getCity();
+		 
+	 }
+	 
+	 public void setLocalVenueState(String state) {
+		 
+		 model.setState(state);
+		 
+	 }
+	 
+	 public String getLocalVenueState() {
+		 
+		 return model.getState();
+		 
+	 }
+	 
+	 public void setLocalVenueZip(String zip) {
+		 
+		 model.setZip(zip);
+		 
+	 }
+	 
+	 public String getLocalVenueZip() {
+		 
+		 return model.getZip();
+		 
+	 }
+	 
+	 public void setLocalVenueLongitude(String longitude) {
+		 
+		 model.setLongitude(longitude);
+		 
+	 }
+	 
+	 public String getLocalVenueLongitude() {
+		 
+		 return model.getLongitude();
+		 
+	 }
+	 
+	 public void setLocalVenueLatitude(String latitude) {
+		 
+		 model.setLatitude(latitude);
+		 
+	 }
+	 
+	 public String getLocalVenueLatitude() {
+		 
+		 return model.getLatitude();
+		 
+	 }
+	 
+	 public void setLocalVenueWebAddress(String webAddress) {
+		 
+		 model.setWebAddress(webAddress);
+		 
+	 }
+	 
+	 public String getLocalVenueWebAddress() {
+		 
+		 return model.getWebAddress();
+		 
+	 }
+
+}
---
+++ Backend/JavaFiles/VenueManagment/src/main/java/com/brownsoundtech/localmusic/venuemanagment/view/LocalVenueView.java
@@ -0,0 +1,17 @@
+package com.brownsoundtech.localmusic.venuemanagment.view;
+
+import com.brownsoundtech.localmusic.venuemanagment.controller.LocalVenueController;
+
+/**
+ * @author brown
+ *
+ */
+public class LocalVenueView {
+	
+	public void printVenueInformation(LocalVenueController venueController) {
+		
+		
+		
+	}
+
+}
