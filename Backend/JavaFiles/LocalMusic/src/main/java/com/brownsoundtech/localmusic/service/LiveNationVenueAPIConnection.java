6e28fc2b23c67b39cd301c8e1bce308bb7512644
William Brown <brownwc86@gmail.com>
2020-08-03 00:27:09 -0400

    Moving Forward with Completing API Connection

--- Backend/JavaFiles/LocalMusic/src/main/java/com/brownsoundtech/localmusic/service/LiveNationVenueAPIConnection.java
+++ Backend/JavaFiles/LocalMusic/src/main/java/com/brownsoundtech/localmusic/service/LiveNationVenueAPIConnection.java
@@ -1,8 +1,6 @@
 
 package com.brownsoundtech.localmusic.service;
 
-
-
 /**
  * @author William Chad Brown
  * @version 0.0.1
@@ -13,44 +11,29 @@ package com.brownsoundtech.localmusic.service;
  */
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.Data;
-
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-
 import com.brownsoundtech.localmusic.domain.LiveNationVenue;
 import com.fasterxml.jackson.core.type.TypeReference;
 
-
+@Data
 public class LiveNationVenueAPIConnection {
 	
 	private ObjectMapper mapper;
 	private TypeReference<List<LiveNationVenue>> typeReference;
 	private String url;
-
-	
-	//This value is pulled from a vault that contains all the keys needed to connect to the systems.
-	private String LiveNationAPIKey;
-	
-	
+		
 	/**
 	 * 
 	 * This method is used to send the request to the API and send the response information to the correct 
 	 * class.  
 	 * 
-	 * TODO: The current setup just prints out the information that is passed. Used for testing at the 
-	 * moment. Still need to verify that the key is pulled from the vault then that the correct 
-	 * URL string is created. Then set up the API connection and then send the response to be parsed. 
-	 * 	 
 	 */
-	public LiveNationVenueAPIConnection() {
+	public LiveNationVenueAPIConnection(String apiKey, String requestedInformation) {
 		
-		System.out.println(LiveNationAPIKey);
+		System.out.println(apiKey);
+		buildRequestString(requestedInformation, apiKey);
+		System.out.println(url);
 	}
 	
 	/**
@@ -62,12 +45,11 @@ public class LiveNationVenueAPIConnection {
 	 * @param request - A formated string that will be used as a URL to connect to an API
 	 * 
 	 */
-	private void buildRequestString(String request) {
+	private void buildRequestString(String request, String key) {
 		
 		String discovery = "https://app.ticketmaster.com/discovery/v2/venues.json?";
-		//String apiKey = "&apikey=" + key;
-		
-		//url = discovery + request + apiKey;
+		String apiKey = "&apikey=" + key;
+		url = discovery + request + apiKey;
 		
 	}
 	
